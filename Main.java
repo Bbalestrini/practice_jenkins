41bd8be11ad1bb49410f21f1b05a32d48d42feaa
Bbalestrini <devops1265@protonmail.me>
2023-06-09 09:10:44 +0200

    the f* chickens

--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ class Main{
 
 	public static void main(String[] args){
 		System.out.println("Hola mundo, desde java");
-		System.out.println("La conexión con Github se ha hecho");
+		System.out.println("La conexión con Github se ha hecho 2");
 	}	
 }
